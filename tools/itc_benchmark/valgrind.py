704508c08cfb75d7ea7c45b6bdb4a504f1a128ce
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-01-27 10:39:41 -0600

    adding suppport for considering gcc and valgrind separately

--- tools/itc_benchmark/valgrind.py
+++ tools/itc_benchmark/valgrind.py
@@ -1,72 +1,86 @@
 import os
 import subprocess
-
+import utils.external_info
 from tools.rv_benchmark.tool import Tool
 from utils.external_info import Info
 import progressbar
 import signal
 
+
 class TimeoutException(Exception):
     pass
 
-class Valgrind(Tool):
 
+class Valgrind(Tool):
     def signal_handler(self, signum, frame):
         raise TimeoutException("Timed out!")
 
+    def get_gcc_command(self, cur_dir, file_prefix, temp_dir_name, vflag):
+        cur_path = os.path.join(self.benchmark_path, cur_dir)
+        temp_path = os.path.join(cur_path, temp_dir_name)
+        if not os.path.exists(temp_path):
+            os.mkdir(temp_path)
+
+        relevant_file_path = os.path.join(cur_path, file_prefix + ".c")
+        if not os.path.exists(relevant_file_path):
+            return []
+        bootstrap_file_path = os.path.join(temp_path, file_prefix + "-temp.c")
+        utils.external_info.bootstrap_file(relevant_file_path, bootstrap_file_path, vflag)
+        return ["gcc", "-Werror", "-I" + os.path.join(self.benchmark_path, "include"), "-o",
+                os.path.join(temp_path, file_prefix, "-temp.out"), bootstrap_file_path]
+
+    def get_valgrind_command(self, cur_dir, file_prefix, temp_dir_name):
+        return ["valgrind", "--exit-errorcode=10",
+                os.path.join(self.benchmark_path, temp_dir_name, file_prefix + "-temp.out")]
+
     def run(self, verbose=False, log_location=None):
+        relevant_dirs = ["01.w_Defects", "02.wo_Defects"]
         output_dict = {}
-        spec_dict = self.info.get_spec_dict()
-        os.chdir(self.benchmark_path)
-        for i in range(1, len(spec_dict.keys()) + 1):
-            if i not in output_dict:
-                output_dict[i] = {"count": spec_dict[i]["count"], "TP": 0, "FP": 0}
-            print self.name + " being tested on file " + str(i)
-            #bar = progressbar.ProgressBar()
-            for j in range(1, spec_dict[i]["count"]):
-                arg = [str('%03d' % i) + str('%03d' % j)]
-                try:
-                    signal.signal(signal.SIGALRM, self.signal_handler)
-                    signal.alarm(10)
-                    output_w = subprocess.check_output(["valgrind", "--error-exitcode=20", "./01.w_Defects/01_w_Defects"] + arg)#, stderr=subprocess.STDOUT)
-                except subprocess.CalledProcessError:
-                    output_dict[i]["TP"] += 1
-                except TimeoutException:
-                    pass
-                try:
-                    signal.alarm(10)
-                    output_wo = subprocess.check_output(["valgrind", "--error-exitcode=20", "./02.wo_Defects/02_wo_Defects"] + arg)#, stderr=subprocess.STDOUT)
-                except subprocess.CalledProcessError:
-                    output_dict[i]["FP"] += 1
-                except TimeoutException:
-                    continue
-                finally:
-                    #reset the alarm
-                    signal.alarm(0)
-                if verbose:
-                    whole_path = os.path.expanduser(log_location)
-                    mode = 'a'
-                    if not os.path.exists(whole_path) or (i == 1 and j == 0):
-                        mode = 'w+'
-                    with open(whole_path, mode) as output_file:
-                        output_file.write(output_w)
-                        output_file.write(output_wo)
+        log_file = None
+        if log_location is not None:
+            log_file = open(log_location, 'w+')
+
+        for cur_dir in relevant_dirs:
+            spec_dict = self.info.get_spec_dict()
+            mapping_dict = self.info.get_file_mapping()
+            for i in range(1, len(spec_dict.keys()) + 1):
+                if i not in output_dict:
+                    output_dict[i] = {"count": spec_dict[i]["count"], "TP": 0, "FP": 0}
+                file_prefix = mapping_dict[i]
+                print self.name + " being tested on folder " + cur_dir + " and file " + file_prefix + ".c"
+                # bar = progressbar.ProgressBar(redirect_stdout=True)
+                for j in range(1, spec_dict[i]["count"]):
+                    vflag = str('%03d' % j)
+                    try:
+                        gcc_command = self.get_gcc_command(cur_dir, file_prefix, "bootstrap_dir", vflag)
+                        print gcc_command
+                        if len(gcc_command) != 0:
+                            signal.signal(signal.SIGALRM, self.signal_handler)
+                            signal.alarm(30)
+                            subprocess.check_output(gcc_command, stderr=subprocess.STDOUT)
+                            valgrind_command = self.get_valgrind_command(cur_dir, file_prefix, "bootstrap_dir")
+                            subprocess.check_output(valgrind_command, stderr=subprocess.STDOUT)
+
+                    except subprocess.CalledProcessError:
+                        if "w_Defects" in cur_dir:
+                            output_dict[i]["TP"] += 1
+                        else:
+                            output_dict[i]["FP"] += 1
+
+
+                    except TimeoutException:
+                        continue
+                    finally:
+                        signal.alarm(0)
         return output_dict
 
     def get_name(self):
         return self.name
 
-
     def __init__(self, benchmark_path):
-        os.chdir(os.path.expanduser(benchmark_path))
-        subprocess.check_call(["./bootstrap"])
-        subprocess.check_call(["automake"])
-        subprocess.check_call(["autoconf"])
-        subprocess.check_call(["./configure"])
-        subprocess.check_call(["make"])
         self.info = Info()
         self.benchmark_path = benchmark_path
-        self.name = "Valg"
+        self.name = "Compcert"
 
     def analyze(self):
         Tool.analyze(self)
